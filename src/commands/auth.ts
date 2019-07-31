af386c7ede469dfff6b61f299282fdeb9b7f99c4
Hao Liu <mouse.mliu@gmail.com>
2019-07-31 22:05:24 +0800

    fix bug for login command

--- src/commands/auth.ts
+++ src/commands/auth.ts
@@ -5,6 +5,10 @@ export async function login() {
     let projectName = await vscode.window.showInputBox({
         placeHolder: "Please input your project name..."
     });
+    
+    if (!projectName) {
+        return vscode.window.showErrorMessage("projectName is required");
+    }
 
     startServer(projectName).then(function(message: any) {
         console.log(message);
