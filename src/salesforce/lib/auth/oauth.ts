0c2983b0173f0a66095a8434f653c040c27329af
Hao Liu <mouse.mliu@gmail.com>
2019-08-04 00:07:47 +0800

    add oauth2 support, remove dependency lib

---
+++ src/salesforce/lib/auth/oauth.ts
@@ -0,0 +1,74 @@
+
+import { port, entryPoint, appConfig } from "./config";
+import * as querystring from "querystring";
+import * as request from "request-promise";
+
+export class OAuth {
+    private authorizeUrl: string;
+    private tokenUrl: string;
+    private revokeUrl: string;
+
+    public constructor(options: any={}) {
+        let loginUrl = options["loginUrl"];
+
+        this.tokenUrl = `${loginUrl}/services/oauth2/token`;
+        this.revokeUrl = `${loginUrl}//services/oauth2/revoke`;
+        this.authorizeUrl = `${loginUrl}/services/oauth2/authorize`;
+    }
+
+    public getAuthorizationUrl(parms?: any) {
+        let params = {
+            response_type: "code",
+            client_id: appConfig["clientId"],
+            redirect_uri: appConfig["redirectUri"]
+        };
+        return `${this.authorizeUrl}?` + querystring.stringify(params);
+    }
+
+    private _invokeTokenRequest(params: any) {
+        let self = this;
+
+        return new Promise(function(resolve, reject) {
+            let requestOptions = {
+                method: "POST",
+                headers : {
+                    "content-type" : "application/x-www-form-urlencoded"
+                },
+                uri: self.tokenUrl,
+                resolveWithFullResponse: true,
+                body: querystring.stringify(params)
+            };
+
+            request(requestOptions).then(response => {
+                resolve(response);
+            })
+            .catch (err => {
+                reject(err);    
+            });
+        });
+    }
+
+    public requestToken(code: string) {
+        let params = {
+            grant_type: "authorization_code",
+            code: code,
+            client_id: appConfig["clientId"],
+            client_secret: appConfig["clientSecret"],
+            redirect_uri: appConfig["redirectUri"]
+        };
+        
+        return this._invokeTokenRequest(params);
+    }
+
+    public refreshToken(refreshToken: string) {
+        let params = {
+            response_type: "refresh_token",
+            refresh_token : refreshToken ,
+            client_id: appConfig["clientId"]
+        };
+
+        return this._invokeTokenRequest(params);
+    }
+
+    public revokeToken(token: string) {}
+}
