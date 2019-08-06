d416f885d66f0f998824ea43da717748d119c6f4
Hao Liu <mouse.mliu@gmail.com>
2019-08-07 00:54:15 +0800

    Finish basic project creation command

--- src/commands/main.ts
+++ src/commands/main.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
+import * as os from "os";
+import * as AdmZip from "adm-zip";
+import * as shelljs from "shelljs";
 import * as util from "../utils/util";
 import { projectSettings } from "../settings";
 import { MetadataApi } from "../salesforce/api/metadata";
@@ -15,10 +18,38 @@ export function createProject() {
 
     let metadataApi = new MetadataApi(sessionInfo);
     metadataApi.retrieve({"types": _types}).then( result => {
-        let zipFilePath = path.join(util.getProjectPath(), "haoide.zip");
+        let projectPath = util.getProjectPath();
+        let zipFilePath = path.join(os.homedir(), "haoide.zip");
         let writeResult = fs.writeFileSync(
             zipFilePath, result["zipFile"], "base64"
         );
-        console.log(writeResult);
+        
+        let zip = new AdmZip(zipFilePath);
+        var zipEntries = zip.getEntries();
+        console.log(zipEntries);
+
+        for (const zipEntry of zipEntries) {
+            let entryName = zipEntry.entryName.replace("unpackaged", "src");
+            let fileName = zipEntry.name;
+
+            // Get file path for every file
+            let filePath = path.join(
+                projectPath, 
+                entryName.substr(0, entryName.lastIndexOf(fileName) - 1)
+            );
+            console.log(filePath);
+            
+            // If folder is not exist, just make it
+            // https://stackoverflow.com/questions/31645738/how-to-create-full-path-with-nodes-fs-mkdirsync
+            if (!fs.existsSync(filePath)) {
+                shelljs.mkdir("-p", filePath);
+            }
+
+            // Write file to local disk
+            fs.writeFileSync(
+                path.join(filePath, fileName),
+                zipEntry.getData()
+            );
+        }
     });
 }
